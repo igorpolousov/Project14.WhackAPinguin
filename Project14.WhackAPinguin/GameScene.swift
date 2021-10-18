dacf749b76f5e36749a85f3ce94b20f88609be36
Igor Polousov <igorpolousov@gmail.com>
2021-10-18 12:56:54 +0300

    + backGround picture + gameScore label

--- Project14.WhackAPinguin/GameScene.swift
+++ Project14.WhackAPinguin/GameScene.swift
@@ -8,9 +8,28 @@
 import SpriteKit
 
 
-class GameScene: SKScene {    
+class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "score = \(score)"
+        }
+    }
 
     override func didMove(to view: SKView) {
+        let backGround = SKSpriteNode(imageNamed: "whackBackGround")
+        backGround.position = CGPoint(x: 512, y: 384)
+        backGround.blendMode = .replace
+        backGround.zPosition = -1
+        addChild(backGround)
+        
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score : 0"
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
         }
         
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
